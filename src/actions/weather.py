6d62b14427d5ff4abb3ddd81a2462ce63091a31a
thomaswtsang <thomaswtsang@gmail.com>
2014-05-03 01:59:16 -0700

    add methods to get current location through IP address in weather module

--- src/actions/weather.py
+++ src/actions/weather.py
@@ -5,8 +5,28 @@ weather actions for alfred using mashery
 import urllib2
 import re
 import json
+
+# yaml module used to convert unicode to ascii
 import yaml
 import string
+import unirest
+
+def get_ip_address():
+    url = 'http://bot.whatismyipaddress.com'
+    response = urllib2.urlopen(url)
+    ip_address = response.read()
+    return ip_address
+
+def get_location(ip_address):
+    response = unirest.get("https://community-telize-json-ip-and-geoip.p.mashape.com/geoip/"
+    + str(ip_address),
+
+        headers={
+            "X-Mashape-Authorization": "W5slDXnAej8yvytZaE3mkrPnZIoQsgcU"
+        }
+    );
+
+    return yaml.load(response.body['city'])
 
 def fill_in_space(unprocessed_text):
     location = re.sub(' ', '+', unprocessed_text)
@@ -23,7 +43,7 @@ def get_json_data(location):
     return json_data
 
 def get_f_degrees(weather_json):
-    print(weather_json)
+    #print(weather_json)
     temp_f = yaml.load(weather_json['data']['current_condition'][0]['temp_F'])
     return temp_f
 
@@ -31,19 +51,19 @@ def get_weather_desc(weather_json):
     desc = yaml.load(weather_json['data']['current_condition'][0]['weatherDesc'][0]['value'])
     return desc
 
-def handler(text, alfred):
-    if(re.search("in|for", text)):
+def handle(message):
+    if(re.search("in|for", message)):
         # "what's the weather in/for/at LA?"
-        tokens = text.split()
+        tokens = message.split()
         for i in range(0,len(tokens)):
             #TODO try \b(in|for|at)\b
             if (re.search("in|for", tokens[i])):
                 unprocessed_location = string.join(tokens[i+1:len(tokens)])
                 break
+
     else:
         # "what's the weather?"
-        #TODO get current location
-        unprocessed_location = "Los Angeles"
+        unprocessed_location = get_location(get_ip_address())
 
     location = fill_in_space(unprocessed_location)
     weather_json = get_json_data(location)
@@ -51,5 +71,5 @@ def handler(text, alfred):
     # exception KeyError for bad return data
     weather_desc = get_weather_desc(weather_json)
 
-    message = "The weather for " + unprocessed_location + " is " + str(temp_f) + " degrees fahrenheit and " + weather_desc
-    alfred.says(message)
+    response = "The weather for " + unprocessed_location + " is " + str(temp_f) + " degrees fahrenheit and " + weather_desc
+    return response
