a471980ba34a0f400ea84b15e5d06f364a8287db
thomaswtsang <thomaswtsang@gmail.com>
2014-05-20 12:58:56 -0400

    add wind direction query capability

--- src/actions/weather.py
+++ src/actions/weather.py
@@ -1,6 +1,13 @@
 
 """
 weather actions for alfred using mashery
+
+sample queries:
+
+what is the weather in New York
+what is the weather for tomorrow (implied current location)
+what is the weather for this Friday
+what is the weather in New York this Friday
 """
 import urllib2
 import re
@@ -11,6 +18,8 @@ import yaml
 import string
 import unirest
 
+import pprint
+
 def get_ip_address():
     url = 'http://bot.whatismyipaddress.com'
     response = urllib2.urlopen(url)
@@ -18,13 +27,13 @@ def get_ip_address():
     return ip_address
 
 def get_location(ip_address):
-    response = unirest.get("https://community-telize-json-ip-and-geoip.p.mashape.com/geoip/"
+    response = unirest.get("https://community-telize-json-ip-and-geoip.p.mashape.com/geoip/" \
     + str(ip_address),
 
         headers={
             "X-Mashape-Authorization": "W5slDXnAej8yvytZaE3mkrPnZIoQsgcU"
         }
-    );
+    )
 
     return yaml.load(response.body['city'])
 
@@ -32,20 +41,64 @@ def fill_in_space(unprocessed_text):
     location = re.sub(' ', '+', unprocessed_text)
     return location
 
-def get_json_data(location):
-    url = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=" + location + "&format=json&num_of_days=1&key=c6y27sgvkadpycg54dgh8z2e"
+def get_json_data(unprocessed_location):
+    location = fill_in_space(unprocessed_location)
+    url = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=" \
+          + location \
+          + "&format=json&num_of_days=5&key=c6y27sgvkadpycg54dgh8z2e"
     response = urllib2.urlopen(url)
     response_data = response.read()
 
     # return json data for other modules to parse
     json_data = json.loads(response_data)
 
+    # debug
+    pp = pprint.PrettyPrinter()
+    pp.pprint(json_data)
+
     return json_data
 
-def get_f_degrees(weather_json):
-    #print(weather_json)
-    temp_f = yaml.load(weather_json['data']['current_condition'][0]['temp_F'])
-    return temp_f
+def get_date(day, pos):
+    """1
+        get date from day and POS tag
+        ie, this Tuesday, next Friday
+
+        input should be "Tuesday", "JJ"
+        where "JJ" refers to "next"
+        and where "DT" refers to "this"
+    """
+
+
+
+def get_current_temp(weather_json):
+    """
+        returns temperature in Fahrenheit for current day
+    """
+    temp = yaml.load(weather_json['data']['current_condition'][0]['temp_F'])
+    return temp
+
+def get_other_temp(weather_json, date):
+    """
+        return the high and low temperature in Fahrenheit for other days
+    """
+
+def get_wind_speed(weather_json):
+    speed = yaml.load(weather_json['data']['current_condition']['windspeedMiles'])
+    return speed
+
+def get_wind_dir(weather_json):
+    temp = yaml.load(weather_json['data']['current_condition']['winddir16Point'])
+    if (temp == 'N'):
+        direction = "north"
+    elif (temp == 'E'):
+        direction = "east"
+    elif (temp == 'W'):
+        direction = "west"
+    elif (temp == 'S'):
+        direction = "south"
+
+    return direction
+
 
 def get_weather_desc(weather_json):
     desc = yaml.load(weather_json['data']['current_condition'][0]['weatherDesc'][0]['value'])
@@ -65,9 +118,8 @@ def handle(message):
         # "what's the weather?"
         unprocessed_location = get_location(get_ip_address())
 
-    location = fill_in_space(unprocessed_location)
-    weather_json = get_json_data(location)
-    temp_f = get_f_degrees(weather_json)
+    weather_json = get_json_data(unprocessed_location)
+    temp_f = get_current_temp(weather_json)
     # exception KeyError for bad return data
     weather_desc = get_weather_desc(weather_json)
 
