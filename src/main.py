1baf6723b5c51d1a30ad1eb37b51ecfc562abbb4
thomaswtsang <thomaswtsang@gmail.com>
2014-11-08 10:54:19 +0000

    removed extra loop for nonparsable speech

--- src/main.py
+++ src/main.py
@@ -6,10 +6,11 @@ if __name__ == '__main__':
 
     alfred = Alfred()
     mic = Mic()
+    
 
     while 1:
         try:
-            alfred.says("how can i help you?")
+            #alfred.says("how can i help you?")
 
             # try to have it as
             # text = mic.record() 
@@ -18,19 +19,21 @@ if __name__ == '__main__':
             mic.write_to_file(sample_width,sound_data)
             text = mic.get_text_from_google()
 
-            print(text)
+            #print(text)
             if(alfred.is_summoned(text)):
                 alfred.says("okay, hold on")
-                while 1:
-                    if (alfred.respond_to(text)):
-                        break
-                    else:
-                        alfred.says("pardon me?")
-                        sample_width, sound_data = mic.record()
-                        mic.write_to_file(sample_width,sound_data)
-                        text = mic.get_text_from_google()
-                        print(text)
+                alfred.respond_to(text)
+                #while 1:
+                #    if (alfred.respond_to(text)):
+                #        break
+                #    else:
+                #        alfred.says("pardon me?")
+                #        sample_width, sound_data = mic.record()
+                #        mic.write_to_file(sample_width,sound_data)
+                #        text = mic.get_text_from_google()
+                #        print(text)
 
 
         except NoParsableSpeech as e:
             print(e.value)
+            alfred.says("please repeat")
